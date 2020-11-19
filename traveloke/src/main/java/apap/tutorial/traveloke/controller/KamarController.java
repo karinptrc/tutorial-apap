c79c5a193f900979cfe69b02251c771c67b622a8
Karin Patricia <karinptrc@gmail.com>
2020-11-19 10:42:58 +0700

    Feat/tutorial 4 traveloke: fix add kamar

--- traveloke/src/main/java/apap/tutorial/traveloke/controller/KamarController.java
+++ traveloke/src/main/java/apap/tutorial/traveloke/controller/KamarController.java
@@ -10,12 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.PostLoad;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -29,37 +29,48 @@ public class KamarController {
 
     public List<KamarModel> tempKamar;
 
-    @GetMapping("/kamar/add/{idHotel}")
-    private String addKamarFormPage(
-            @PathVariable Long idHotel,
-            Model model
-    ){
-        HotelModel hotel = hotelService.getHotelByIdHotel(idHotel);
-        hotel.setListKamar(tempKamar);
+    @RequestMapping(value = "/kamar/add/{idHotel}")
+    private String add(@PathVariable(value = "idHotel") Long idHotel, Model model) {
+        model.addAttribute("titleTab", "Add Kamar");
+        HotelModel hotel = new HotelModel();
+        hotel.setListKamar(new ArrayList<KamarModel>());
         hotel.getListKamar().add(new KamarModel());
         model.addAttribute("hotel", hotel);
-        model.addAttribute("page", "Add Kamar");
-        return "form-add-kamar";
-
-    }
-
-    @PostMapping("/kamar/add/{idHotel}")
-    private String addRow(
-            @ModelAttribute HotelModel hotel,
-            Model model
-    ){
-        model.addAttribute("tempKamar", tempKamar);
+        model.addAttribute("idHotel", idHotel);
         return "form-add-kamar";
     }
 
-    @PostMapping("/kamar/add")
-    private String addKamarSubmit(
-            @ModelAttribute KamarModel kamar,
-            Model model
-    ){
-        kamarService.addKamar(kamar);
-        model.addAttribute("kamar", kamar);
+    @RequestMapping(value = "/kamar/add/{idHotel}", method = RequestMethod.POST, params= {"save"})
+    private String addMenuSubmit(@PathVariable(value = "idHotel") Long idHotel, @ModelAttribute HotelModel hotel, ModelMap model) {
+        model.addAttribute("titleTab", "Add Menu");
+        HotelModel oldHotel = hotelService.getHotelByIdHotel(idHotel);
+        model.addAttribute("kamarCount", hotel.getListKamar().size());
+
+        model.addAttribute("idHotel", idHotel);
+        for (KamarModel kamar : hotel.getListKamar()) {
+            kamar.setHotel(oldHotel);
+            kamarService.addKamar(kamar);
+        }
         return "add-kamar";
+    }
+
+    @RequestMapping(value = "/kamar/add/{idHotel}", method = RequestMethod.POST, params={"addRow"})
+    private String addRow(@PathVariable(value = "idHotel") Long idHotel, @ModelAttribute HotelModel hotel, Model model) {
+        model.addAttribute("titleTab", "Add Menu");
+        KamarModel kamar = new KamarModel();
+        hotel.getListKamar().add(kamar);
+        model.addAttribute("hotel", hotel);
+        model.addAttribute("idHotel", idHotel);
+        return "form-add-kamar";
+    }
+
+    @RequestMapping(value = "/kamar/add/{idHotel}", method = RequestMethod.POST, params={"removeRow"})
+    private String removeRow(@PathVariable(value = "idHotel") Long idHotel,@ModelAttribute HotelModel hotel, Model model, HttpServletRequest req) {
+        Integer rowId =  Integer.valueOf(req.getParameter("removeRow"));
+        hotel.getListKamar().remove(rowId.intValue());
+        model.addAttribute("hotel", hotel);
+        model.addAttribute("idHotel", idHotel);
+        return "form-add-kamar";
     }
 
     @GetMapping("/kamar/change/{noKamar}")
